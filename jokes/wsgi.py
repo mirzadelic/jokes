4b0e99474d29c382396a3f0d3cb1e98b9800ea75
Mirza Delic <mirzadelic1@gmail.com>
2016-03-21 23:47:19 +0100

    wsgi

--- jokes/wsgi.py
+++ jokes/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jokes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jokes.deployment")
 
 application = get_wsgi_application()
