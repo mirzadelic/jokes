d9f9eb8ef7e710af089f612df5b1d35ebf66a028
Mirza Delic <mirzadelic1@gmail.com>
2016-05-18 19:31:24 +0200

    fix

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewsets, filters, status
+from rest_framework import serializers, viewsets, status
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from models import *
